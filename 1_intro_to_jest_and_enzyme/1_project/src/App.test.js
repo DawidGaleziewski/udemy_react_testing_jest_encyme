d512a1b1e23cc49bcaaeab5538913d8719e97025
Dawid Gałęziewski <Dawidgalsino@vp.pl>
2021-01-24 19:38:14 +0100

    update: learned how to setup enzyme and jest

--- 1_intro_to_jest_and_enzyme/1_project/src/App.test.js
+++ 1_intro_to_jest_and_enzyme/1_project/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { EnzymeAdapter } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import App from './App';
 
+//  Configure enzyme (letter in seperate file)
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+
+
 test('renders learn react link', () => {
-  render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+ 
 });
