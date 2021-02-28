72dbe46208d6d20c0905d689f7379fb356ab09e6
Dawid Gałęziewski <Dawidgalsino@vp.pl>
2021-02-28 19:04:42 +0100

    finish for today

---
+++ 3_jotto_app/jotto_appclear/src/helpers/index.js
@@ -0,0 +1,9 @@
+/**
+ * @method getLetterMatchCount
+ * @param {string} guessedWord 
+ * @param {string} secretWord 
+ * @returns {number} - Number of letters matched between guessed word and secret word
+ */
+export function getLetterMatchCount(guessedWord, secretWord){
+
+}
