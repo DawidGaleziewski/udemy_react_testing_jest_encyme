a7249eccd80295fee4653962ee25fc8ac7e6c4ae
Dawid Gałęziewski <Dawidgalsino@vp.pl>
2021-03-08 21:39:55 +0100

    learned how to mock function call events

--- 4_testingHooks/jotto_appclear/src/Input/Input.js
+++ 4_testingHooks/jotto_appclear/src/Input/Input.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-const Input = (props) => {
+const Input = ({secretWord}) => {
+    // It is not destructured so that we can test it!
+    const [currentGuess, setCurrentGuess] = React.useState("");
+
     return <div data-test="component-input"> 
+    <form className="form-inline">
+        <input data-test="input-box" className="mb-2 mx-sm-2"
+        type="text"
+        placeholder="enter guess"
+        value={currentGuess}
+        onChange={(event)=> {
+            setCurrentGuess(event.target.value)
+        }}
+        />
+
+        <button data-test="submit-button" className="btn btn-primary mb-2">
+            Submit
+        </button>
+    </form>
 
     </div>
 }
--- 4_testingHooks/jotto_appclear/src/Input/Input.test.js
+++ 4_testingHooks/jotto_appclear/src/Input/Input.test.js
@@ -17,3 +17,25 @@ test('did component render', ()=> {
 test('component accepts correct prop types', ()=> {
     checkProps(Input, {secretWord: 'Monkey'})
 })
+
+describe('state controlled input field', ()=> {
+    test('state updates with value of input box upon change', ()=> {
+        const mockSetCurrentGuess = jest.fn();
+        // We replace useState with another mock
+        // This function, when test runs will replace the hook returning intialState and setState function to our custome ones
+        React.useState = jest.fn(()=> ["", mockSetCurrentGuess])
+
+        const wrapper = setup();
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+
+        //  We create a mock event that will simulate the change on the form
+        const mockEvent = {target: {value: 'train'}};
+        //  Then we simulate the input box getting a value of 'train'
+        inputBox.simulate("change", mockEvent);
+
+        // We can check if our mock function have been called with the correct value
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train')
+
+        // Function should go red with "Number of calls: 0" as the function was not called
+    })
+})
